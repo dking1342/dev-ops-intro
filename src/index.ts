a35b945af47bbc23b307e2d5f80a7eba7e45055f
Kavooce <kavooce.cheetah@gmail.com>
2022-08-11 21:22:27 -0500

    typescript setup

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  return res.json({success:true, payload:"hello world"})
+});
+
+app.listen(4000, ()=> console.log("listening on port 4000"));
