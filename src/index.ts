8a61119377dabd163835043bbdddf7a2eb93ca27
Kavooce <kavooce.cheetah@gmail.com>
2022-08-11 21:53:56 -0500

    made docker compose file for development

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
-  return res.json({success:true, payload:"hello world"})
+  return res.json({success:true, payload:"hello world!"})
 });
 
 app.listen(4000, ()=> console.log("listening on port 4000"));
