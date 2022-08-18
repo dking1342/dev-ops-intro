3892e15b271e886500822ccd2759df4a472e0d97
Kavooce <kavooce.cheetah@gmail.com>
2022-08-17 21:06:01 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Terraform Crash Course
+
+## Documentation
+Documentation for Terraform can be found [here](https://www.terraform.io/). A video documentation can be found [here](https://www.youtube.com/watch?v=SLB_c_ayRMo&list=WL&index=2&t=120s).
+
+## Explanation
+This crash course covers basics and fundamentals of Terraform. 
+
+### Terraform Setup Tutorials
+Terraform has made tutorials that guide you through the workflow with a provider. To see this visit [here](https://learn.hashicorp.com/terraform?utm_source=terraform_io)
+
---
+++ main.tf
@@ -0,0 +1,51 @@
+provider "google" {
+  project = "acme-app"
+  region  = "us-central1"
+}
+
+# The default provider configuration; resources that begin with `aws_` will use
+# it as the default, and it can be referenced as `aws`.
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Additional provider configuration for west coast region; resources can
+# reference this as `aws.west`.
+provider "aws" {
+  alias  = "west"
+  region = "us-west-2"
+}
+
+resource "aws_instance" "myapp" {
+  ami           = "ami-832948"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+#####
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
